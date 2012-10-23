cab60d53f5594ce33054d1628946539c142f1770
LIM KIM <rakuenz.12@gmail.com>
2012-10-23 15:14:06 +0800

    Added Arduino Coding and Processing Coding

---
+++ Processing Coding/sample_under_work.pde
@@ -0,0 +1,149 @@
+// Sample Control for Processing V.90
+// Description: Loads and manipulates the Pitch and Panning of a sample object
+// Instructions: Move the mouse Up/Down to control the sample-pitch, and sideways for the panning.
+// By: Amit Pitaru on July 16th 2005
+
+import pitaru.sonia_v2_9.*; // automatically created when importing sonia using the processing menu.
+Sample mySample;
+
+import processing.serial.*;
+Serial myPort;
+float value_x = 0;
+float value_y = 0;
+
+float [] valueArrayX;
+float [] valueArrayY;
+
+float avg_X;
+float avg_Y;
+
+int countIndex = 0;
+int recordInterval = 5000;
+int recordTime = 10;
+
+int currentTime = 0;
+int previousTime = 0;
+
+int i;
+
+void setup(){
+
+size(512,200);
+  Sonia.start(this); // Start Sonia engine.
+  // create a new sample object.
+  mySample = new Sample("rolling.aif");   
+  
+  String portName = "COM22";
+  myPort = new Serial(this,portName,19200);
+  myPort.bufferUntil('\n');
+  
+  valueArrayX = new float[10];
+  valueArrayY = new float[10];
+}
+
+void draw()
+{
+ background(0,30,0);
+ strokeWeight(1); 
+ 
+ currentTime = millis();
+ if((currentTime - previousTime) > recordInterval){
+   println("assessing run");
+   countIndex = (countIndex + 1) % recordTime;
+   valueArrayX[countIndex] = value_x;
+   valueArrayY[countIndex] = value_y;
+   
+   println("index:"+countIndex+"X:"+","+"Y:"+valueArrayY[countIndex]);
+   
+   for(int i = 0; i < recordTime; i++){
+     avg_X = avg_X + valueArrayX[i];
+     avg_Y = avg_Y + valueArrayY[i];
+   }
+   avg_X = avg_X + valueArrayX[i];
+   avg_Y = avg_Y + valueArrayY[i];
+ }
+ avg_X = avg_X / recordTime;
+ avg_Y = avg_Y / recordTime;
+ previousTime = currentTime;
+
+ 
+ // If sample is playing (or looping), do this...
+ if(mySample.isPlaying()){
+   background(0,40,0);
+ }
+           
+ setRate(); // use mouseY to control sample-rate playback
+ setPan(); // use mouseX to control sample Panning
+ //setVolume(); // use mouseX to control sample volume
+ drawScroller();            
+ 
+ mySample.repeat();
+}
+
+  void serialEvent(Serial p){
+  String s ="";
+  String[] values;
+  
+  if(p.available() > 0){
+    s = p.readString();
+    if(s != null){
+      //println("received:" + s);
+      
+      values = s.split(",");
+      //println(values);
+      
+      value_x = value_x - (value_x - float(values[0]))*0.1; //smoothing
+      value_y = float(values[1]);
+    }
+  }
+}
+
+
+void setPan(){
+  // set the pan of the sample object.
+  // Range: float from -1 to 1 .... -1 -> left, 0 -> balanced ,1 -> right
+  // notes: only works with MONO samples. Pan for Stereo support in next version.
+  float pan = -1f + avg_X/(width/2f);
+  mySample.setPan(pan);
+
+}
+
+// NOT IN USE FOR THIS EXAMPLE
+void setVolume(){
+  // set the volume of the sample. 
+  // Range: float from 0 to 1 
+  float vol = avg_Y/(height*1f);
+  mySample.setVolume(vol);
+}
+
+void setRate(){
+   // set the speed (sampling rate) of the sample.
+   // Values:
+   // 0 -> very low pitch (slow playback).
+   // 88200 -> very high pitch (fast playback).
+   float rate = (height - avg_Y)*88200/(height);
+   mySample.setRate(rate);
+}
+
+// Draw a scroller that shows the current sample-frame being played.
+// Notice how the sample plays faster when the Sample-Rate is higher.(controlled by mouseY)
+
+void drawScroller(){
+ strokeWeight(1);
+ stroke(0,255,0);
+
+ // figure out which percent of the sample has been played.
+ float percent = mySample.getCurrentFrame() *100f / mySample.getNumFrames();
+ // calculate the marker position
+ float marker = percent*width/100f;
+ // draw...
+ line(marker,0,marker,20);
+ line(0,10, width,10);
+}
+
+// Safely close the sound engine upon Browser shutdown.
+public void stop(){
+  Sonia.stop();
+  super.stop();
+}
+         
---
+++ arduino coding/attempt_arduino.ino
@@ -0,0 +1,202 @@
+/*
+ * NunchuckPrint
+ *
+ * 2007 Tod E. Kurt, http://todbot.com/blog/
+ *
+ * The Wii Nunchuck reading code is taken from Windmeadow Labs
+ *   http://www.windmeadow.com/node/42
+ */
+ 
+#include <Wire.h>
+
+void setup()
+{
+  Serial.begin(19200);
+  nunchuck_setpowerpins(); // use analog pins 2&3 as fake gnd & pwr
+  nunchuck_init(); // send the initilization handshake
+  Serial.print ("Pacer Activated\n");
+}
+
+void loop()
+{
+  nunchuck_get_data();
+  nunchuck_print_data();
+  delay(100);
+}
+
+
+//
+// Nunchuck functions
+//
+
+static uint8_t nunchuck_buf[6];   // array to store nunchuck data,
+
+// Uses port C (analog in) pins as power & ground for Nunchuck
+static void nunchuck_setpowerpins()
+{
+#define pwrpin PORTC3
+#define gndpin PORTC2
+    DDRC |= _BV(pwrpin) | _BV(gndpin);
+    PORTC &=~ _BV(gndpin);
+    PORTC |=  _BV(pwrpin);
+    delay(100);  // wait for things to stabilize        
+}
+
+// initialize the I2C system, join the I2C bus,
+// and tell the nunchuck we're talking to it
+void nunchuck_init()
+{ 
+  Wire.begin();	                // join i2c bus as master
+  Wire.beginTransmission(0x52);	// transmit to device 0x52
+  Wire.write(0x40);		// sends memory address
+  Wire.write(0x00);		// sends sent a zero.  
+  Wire.endTransmission();	// stop transmitting
+}
+
+// Send a request for data to the nunchuck
+// was "send_zero()"
+void nunchuck_send_request()
+{
+  Wire.beginTransmission(0x52);	// transmit to device 0x52
+  Wire.write(0x00);		// sends one byte
+  Wire.endTransmission();	// stop transmitting
+}
+
+// Receive data back from the nunchuck, 
+int nunchuck_get_data()
+{
+    int cnt=0;
+    Wire.requestFrom (0x52, 6);	// request data from nunchuck
+    while (Wire.available ()) {
+      // receive byte as an integer
+      nunchuck_buf[cnt] = nunchuk_decode_byte(Wire.read());
+      cnt++;
+    }
+    nunchuck_send_request();  // send request for next data payload
+    // If we recieved the 6 bytes, then go print them
+    if (cnt >= 5) {
+     return 1;   // success
+    }
+    return 0; //failure
+}
+
+// Print the input data we have recieved
+// accel data is 10 bits long
+// so we read 8 bits, then we have to add
+// on the last 2 bits.  That is why I
+// multiply them by 2 * 2
+void nunchuck_print_data()
+{ 
+  static int i=0;
+  int joy_x_axis = nunchuck_buf[0];
+  int joy_y_axis = nunchuck_buf[1];
+  int accel_x_axis = nunchuck_buf[2]; // * 2 * 2; 
+  int accel_y_axis = nunchuck_buf[3]; // * 2 * 2;
+  int accel_z_axis = nunchuck_buf[4]; // * 2 * 2;
+
+  int z_button = 0;
+  int c_button = 0;
+  
+  int mid_x = 140; 
+int mid_y = 160;
+  int mid_z = 156;
+  
+  int diff_x = accel_x_axis - mid_x;
+  int diff_y = accel_y_axis - mid_y;
+  int diff_z = accel_z_axis - mid_z;
+  
+  int modulus_x = abs(diff_x);
+  int modulus_y = abs(diff_y);
+  int modulus_z = abs(diff_z);
+  
+  int filtered_modulus_x = modulus_x * 0.3;
+  int filtered_modulus_y = modulus_y * 0.3;
+  int filtered_modulus_z = modulus_z * 0.3;
+  
+  int value_x;
+  int value_y;
+  int value_z;
+  
+  //int sensor_value = (filtered_modulus_y + filtered_modulus_z)/2;
+ // int sensor_val;
+  
+  // byte nunchuck_buf[5] contains bits for z and c buttons
+  // it also contains the least significant bits for the accelerometer data
+  // so we have to check each bit of byte outbuf[5]
+  if ((nunchuck_buf[5] >> 0) & 1) 
+    z_button = 1;
+  if ((nunchuck_buf[5] >> 1) & 1)
+    c_button = 1;
+
+  if ((nunchuck_buf[5] >> 2) & 1) 
+    accel_x_axis += 2;
+  if ((nunchuck_buf[5] >> 3) & 1)
+    accel_x_axis += 1;
+
+  if ((nunchuck_buf[5] >> 4) & 1)
+    accel_y_axis += 2;
+  if ((nunchuck_buf[5] >> 5) & 1)
+    accel_y_axis += 1;
+
+  if ((nunchuck_buf[5] >> 6) & 1)
+    accel_z_axis += 2;
+  if ((nunchuck_buf[5] >> 7) & 1)
+    accel_z_axis += 1;
+
+
+  //Serial.print(accel_x_axis, DEC);
+  //Serial.print(",");
+  //Serial.print(accel_y_axis, DEC);
+  //Serial.print(",");
+  //Serial.print(accel_z_axis, DEC);
+  //Serial.print(",");
+  //Serial.print(z_button, DEC);
+  //Serial.print(",");
+  //Serial.print(c_button, DEC);
+  //Serial.print(",");
+  //Serial.print(modulus_x,DEC);
+  //Serial.print(",");
+  //Serial.print(modulus_y,DEC);
+  //Serial.print(",");
+  //Serial.print(modulus_z,DEC);
+  //Serial.print(",");
+ 
+  //Serial.print(filtered_modulus_x,DEC);
+  //Serial.print(",");
+  //Serial.print(filtered_modulus_y,DEC);
+  //Serial.print(",");
+  //Serial.print(filtered_modulus_z,DEC);
+  //Serial.print(',');
+  //Serial.print(sensor_value,DEC);
+  //Serial.print("\r\n");  // newline
+  
+  value_x = map(filtered_modulus_x,0,20,0,255);
+  value_y = map(filtered_modulus_y,0,20,0,255);
+  value_z = map(filtered_modulus_z,0,20,0,255);
+  //sensor_val = map(sensor_value,0,40,0,255);
+  
+  
+ //Serial.print(sensor_val);
+ 
+  Serial.print(value_x);
+  Serial.print(',');
+  Serial.print(value_y);
+  Serial.print("\n");  // newline
+  
+  //Serial.print(value_x);
+  //Serial.print(',');
+  //Serial.print(value_y);
+  //Serial.print(',');
+  //Serial.print(value_z);
+  //Serial.print('\n');
+  //Serial.print("\r\n");  // newline
+  i++;
+}
+
+// Encode data to format that most wiimote drivers except
+// only needed if you use one of the regular wiimote drivers
+char nunchuk_decode_byte (char x)
+{
+  x = (x ^ 0x17) + 0x17;
+  return x;
+}
